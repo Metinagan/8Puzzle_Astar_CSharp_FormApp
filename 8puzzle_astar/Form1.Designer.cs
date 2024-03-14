f5b2c2c71dd1adb9290f8cb76564699aa8e605df
Metin Agan <130462728+Metinagan@users.noreply.github.com>
2024-03-15 00:59:54 +0300

    final

---
+++ 8puzzle_astar/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace _8puzzle_astar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(55, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(245, 164);
+            this.panel1.TabIndex = 0;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(165, 111);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 48);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(84, 111);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 48);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(3, 111);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 48);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(165, 57);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 48);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(84, 57);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 48);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(3, 57);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 48);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(164, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(55, 12);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(85, 28);
+            this.button10.TabIndex = 1;
+            this.button10.Text = "Step =";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(156, 12);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(85, 28);
+            this.button11.TabIndex = 2;
+            this.button11.Text = "0";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(55, 222);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(115, 35);
+            this.button12.TabIndex = 3;
+            this.button12.Text = "Heuristic Value = ";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(176, 222);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 35);
+            this.button13.TabIndex = 4;
+            this.button13.Text = "0";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(306, 55);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 156);
+            this.button14.TabIndex = 5;
+            this.button14.Text = "Coz";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 269);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+    }
+}
+
---
+++ 8puzzle_astar/Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8puzzle_astar
+{
+    public partial class Form1 : Form
+    {
+        static List<int[,]> mainList = new List<int[,]>();
+        static List<int[]> neighborsList = new List<int[]>();
+        static List<int[,]> posList = new List<int[,]>();
+
+        static int[,] startPos = new int[,] { 
+                                            { 1, 2, 3 }, 
+                                            { 0, 4, 5 },    
+                                            { 7, 8, 6 } };
+        
+        static int[,] endPos = new int[,] { { 1, 2, 3 }, 
+                                            { 4, 5, 6 }, 
+                                            { 7, 8, 0 } };
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mainList.Add(startPos);
+            PrintMatrix(startPos);
+        }
+        static int Hamming(int[,] matrix)
+        {
+            int hammingValue = 0;
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (matrix[x, y] != endPos[x, y])
+                    {
+                        hammingValue++;
+                    }
+                }
+            }
+            return hammingValue;
+        }
+        static int Mannathen(int[,] matrix)
+        {
+            int mannathenValue = 0;
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (matrix[x, y] != 0)
+                    {
+                        for (int k = 0; k < 3; k++)
+                        {
+                            for (int l = 0; l < 3; l++)
+                            {
+                                if (matrix[x, y] == endPos[k, l])
+                                {
+                                    int value = Math.Abs(l - y) + Math.Abs(k - x);
+                                    mannathenValue += value;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return mannathenValue;
+        }
+        static int Heuristic(int[,] matrix)
+        {
+            return Hamming(matrix) + Mannathen(matrix);
+        }
+
+        static Tuple<int, int> FindBlanks(int[,] matrix)
+        {
+            int zerorow = 0;
+            int zerocol = 0;
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    if (matrix[x, y] == 0)
+                    {
+                        zerorow = x;
+                        zerocol = y;
+                    }
+                }
+            }
+            return Tuple.Create(zerorow, zerocol);
+        }
+        static void Neighbors(int[,] matrix)
+        {
+            neighborsList.Clear();
+            var blankLocation = FindBlanks(matrix);
+            int row = blankLocation.Item1;
+            int col = blankLocation.Item2;
+
+            if (row > 0)
+            {
+                neighborsList.Add(new int[] { row - 1, col });
+            }
+            if (row < 2)
+            {
+                neighborsList.Add(new int[] { row + 1, col });
+            }
+            if (col > 0)
+            {
+                neighborsList.Add(new int[] { row, col - 1 });
+            }
+            if (col < 2)
+            {
+                neighborsList.Add(new int[] { row, col + 1 });
+            }
+        }
+
+        static void ChangeBlanks(int[,] matrix, int row, int col)
+        {
+            var blankLocation = FindBlanks(matrix);
+            int zeroRow = blankLocation.Item1;
+            int zeroCol = blankLocation.Item2;
+
+            int newNumber = matrix[row, col];
+            int[,] newNode = (int[,])matrix.Clone();
+            newNode[zeroRow, zeroCol] = newNumber;
+            newNode[row, col] = 0;
+
+            if (!IsMatrixInList(mainList, newNode))
+            {
+                posList.Add(newNode);
+            }
+        }
+        void changebutontex(int value,int ind)
+        {
+            Button button = (Button)panel1.Controls[ind];
+            button.Text=value.ToString();
+        }
+
+
+
+        async Task PrintMatrix(int[,]matrix)
+        {
+            int buttonindex = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    string buttonName = "button" + buttonindex;
+                    Button foundbutton = panel1.Controls.Find(buttonName, true).FirstOrDefault() as Button;
+                    if (foundbutton != null)
+                    {
+                        int value = matrix[i, j];
+                        foundbutton.Text = " ";
+                        foundbutton.Text = value.ToString();
+                    }
+                    buttonindex++;
+                }
+            }
+            await Task.Delay(1500);
+        }
+        static void CreateNeighbors(int[,] matrix)
+        {
+            neighborsList.Clear();
+            int[,] copyMatrix = (int[,])matrix.Clone();
+            Neighbors(copyMatrix);
+
+            foreach (var neighbor in neighborsList)
+            {
+                ChangeBlanks(copyMatrix, neighbor[0], neighbor[1]);
+            }
+        }
+        async Task AStarAsync(int[,] matrix)
+        {
+            posList.Clear();
+            neighborsList.Clear();
+            List<int> heuristicList = new List<int>();
+            int nowheu=Heuristic(matrix);
+            button13.Text = nowheu.ToString();
+            await PrintMatrix(matrix);
+            if (MatrixEquals(matrix, endPos))
+            {
+                Console.WriteLine("bitti");
+            }
+            else
+            {
+                int[,] copyMatrix = (int[,])matrix.Clone();
+                CreateNeighbors(copyMatrix);
+
+                foreach (var newPos in posList)
+                {
+                    if (!IsMatrixInList(mainList, newPos))
+                    {
+                        heuristicList.Add(Heuristic(newPos));
+                    }
+                }
+                int minHeuristic = heuristicList.Min();
+                
+                int step = mainList.Count;
+                button11.Text = step.ToString();
+
+                int mindidex= heuristicList.IndexOf(minHeuristic);
+                mainList.Add(posList[mindidex]);
+
+                
+                
+                AStarAsync(posList[mindidex]);
+                
+            }
+            
+        }
+
+        static bool IsMatrixInList(List<int[,]> matrixList, int[,] matrix)
+        {
+            foreach (var item in matrixList)
+            {
+                if (MatrixEquals(item, matrix))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static bool MatrixEquals(int[,] matrix1, int[,] matrix2)
+        {
+            if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    if (matrix1[i, j] != matrix2[i, j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            mainList.Clear();
+            mainList.Add(startPos);
+            int lenght =mainList.Count;
+            AStarAsync(mainList[lenght-1]);
+        }
+    }
+}
